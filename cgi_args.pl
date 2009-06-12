35d2335d53ec1d099d5e45e8d9d0c80fef5bf5ed
Jamie Cameron <jcameron@webmin.com>
2009-06-12 22:26:35 +0000

    Participate in Webmin search

---
+++ cgi_args.pl
@@ -0,0 +1,29 @@
+
+do 'virtualmin-registrar-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+my ($d) = grep { &virtual_server::can_edit_domain($_) &&
+	         $_->{$module_name} } &virtual_server::list_domains();
+if ($cgi eq 'edit_contact.cgi' || $cgi eq 'edit_dereg.cgi' ||
+    $cgi eq 'edit_ns.cgi' || $cgi eq 'edit_renew.cgi') {
+	# Domain-based form
+	return $d ? 'dom='.&urlize($d->{'dom'}) : 'none';
+	}
+elsif ($cgi eq 'edit.cgi') {
+	# Show first account
+	my @accounts = &list_registrar_accounts();
+	return !$access{'registrar'} ? 'none' :
+	       @accounts ? 'id='.$accounts[0]->{'id'} :
+			   'registrar=rcom';
+	}
+elsif ($cgi eq 'list.cgi') {
+	# Works with no args
+	return '';
+	}
+elsif ($cgi eq 'create_form.cgi') {
+	return 'registrar=rcom';
+	}
+return undef;
+}
